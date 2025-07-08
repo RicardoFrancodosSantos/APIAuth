f6465744af272b5e806637aa98edf8c9d27ccdd4
RicardoFrancodosSantos <ricardofrancosantos@gmail.com>
2025-07-08 17:32:00 -0300

    Inserted basic auth

--- index.js
+++ index.js
@@ -24,28 +24,20 @@ app.get("/noAuth", async (req, res) => {
   }
 });
 
-
-app.get("/noAuth", (req, res) => {
-
-  
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
-});
-
-app.get("/basicAuth", (req, res) => {
-  //TODO 3: Write your code here to hit up the /all endpoint
-  //Specify that you only want the secrets from page 2
-  //HINT: This is how you can use axios to do basic auth:
-  // https://stackoverflow.com/a/74632908
-  /*
-   axios.get(URL, {
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
       auth: {
-        username: "abc",
-        password: "123",
+        username: yourUsername,
+        password: yourPassword,
       },
     });
-  */
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
+  
 
 app.get("/apiKey", (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
